97cf7f6bf7dbfbd41f0e318571aae4275a9bc4d6
MatheusHLT <matheus.honorato.l.teixeira@gmail.com>
2026-03-08 19:28:27 -0300

    Melhorias

--- src/App.java
+++ src/App.java
@@ -11,9 +11,9 @@ public class App {
         double distancia;
         double combustivel;
 
-        System.out.println("Distância percorrida (km):");
+        System.out.print("Distância percorrida (km): ");
         distancia = scanner.nextDouble();
-        System.out.println("Combustível gasto (litros):");
+        System.out.print("Combustível gasto (litros): ");
         combustivel = scanner.nextDouble();
 
         System.out.printf("Consumo médio: %s km/l", (distancia / combustivel));
