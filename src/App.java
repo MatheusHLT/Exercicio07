09ab94328f4f5bb2fac2655486e8f3d2c88342fb
MatheusHLT <matheus.honorato.l.teixeira@gmail.com>
2026-03-08 14:28:09 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double distancia;
+        double combustivel;
+
+        System.out.println("Distância percorrida (km):");
+        distancia = scanner.nextDouble();
+        System.out.println("Combustível gasto (litros):");
+        combustivel = scanner.nextDouble();
+
+        System.out.printf("Consumo médio: %s km/l", (distancia / combustivel));
+
+        scanner.close();
+    }
+}
